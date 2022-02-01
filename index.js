6a30a980a0d01475eddfc2a435c987c050e98af9
Moshe <moisherosenzweig@gmail.com>
2022-02-01 12:24:28 -0500

    commit tree 98136797cbfe7cb206045346fbfa408e0d97175e

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// code your solution here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+    // console.log(`This Saturday, I want to ${activity}`)
+    
+}
+
+saturdayFun("I want to bathe my dog");
+
+function mondayWork(activity='go to the office') {
+return `This Monday, I will ${activity}.`
+}
+
+mondayWork("work from home")
+
+function wrapAdjective (award=special) {
+    const innerFunction = function() {
+return `You are ${award}`
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// (function (baseNumber) {
+//     return baseNumber + 3;
+// })(2);
+
+// console.log(baseNumber)
